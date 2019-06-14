16d48c872d1b45a4e41d85c61c358d67a988964d
sloan58 <martin.sloan@karma-tek.com>
2019-06-14 15:57:59 -0400

    move jitsi numbers to /api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/jitsiDialInNumbers', function() {
+    $response = [
+        'message' => 'Phone numbers available',
+        'numbers' => [
+            'US' => ['+1202509421']
+        ],
+        'numbersEnabled' => true
+    ];
+
+    return $response;
+});
